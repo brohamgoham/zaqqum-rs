9dd15817306b9723a79eb05ab151efb934ae3812
brohamgoham <mohamgoham@gmail.com>
2023-03-23 14:29:51 -0400

    dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+use std::net::TcpListener;
+use tungstenite::server::accept;
+
+
 fn main() {
-    println!("Hello, world!");
+    let server = TcpListener::bind("127.0.0.1").unwrap();
+    for stream in server.incoming() {
+        let stream = stream.unwrap();
+        accept(stream).unwrap().0.for_each(|msg| {
+            let msg = msg.unwrap();
+            println!("Receieved msg")
+        });
+    }
 }
